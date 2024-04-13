1c2fb6f2ca2193dd458c6ec84d99c7b8097c08c2
Jerinel Mendoza Cruz <yeri.paulino@yahoo.es>
2024-04-12 22:12:42 -0400

    primer comit

---
+++ src/main/java/com/mycompany/nominas/anderson/NominasAnderson.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nominas.anderson;
+
+import VentanasPrincipales.principalFrm;
+
+
+
+/**
+ *
+ * @author Alvaro
+ */
+public class NominasAnderson {
+
+      public static void main(String[] args) {
+        
+        principalFrm frm = new principalFrm();
+        frm.setLocationRelativeTo(null);
+        frm.setVisible(true);
+        
+        System.out.println("Hello World!");
+    }
+}
